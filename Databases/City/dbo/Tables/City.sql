71c2b6515858918743264ac91824e6316713cbc2
Vladzimir Kuznetsov <VladzimirKuznetsov@coherentsolutions.com>
2016-11-10 14:03:50 +0300

    gitignore added. CountryID added for City

--- Databases/City/dbo/Tables/City.sql
+++ Databases/City/dbo/Tables/City.sql
@@ -1,6 +1,8 @@
 ﻿CREATE TABLE [dbo].[City] (
     [ID]   INT           IDENTITY (1, 1) NOT NULL,
     [Name] VARCHAR (255) NOT NULL,
-    PRIMARY KEY CLUSTERED ([ID] ASC)
+    [CountryID] INT NOT NULL, 
+    PRIMARY KEY CLUSTERED ([ID] ASC),
+	CONSTRAINT fk_country_id FOREIGN KEY (CountryID) REFERENCES Country(ID)
 );
 
