e42322883c165b7de238813a0fe3fc45f99fa1f8
Vladzimir Kuznetsov <VladzimirKuznetsov@coherentsolutions.com>
2016-11-10 16:28:17 +0300

    MasterDB added

---
+++ Databases/Master/dbo/Tables/Configuration.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Configuration] (
+    [ID]          INT           IDENTITY (1, 1) NOT NULL,
+    [OptionName]  VARCHAR (255) NOT NULL,
+    [OptionValue] VARCHAR (255) NOT NULL,
+    PRIMARY KEY CLUSTERED ([ID] ASC)
+);
+
