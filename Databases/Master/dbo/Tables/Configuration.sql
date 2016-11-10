0ec2a4d0ec755aca0a571637e6d87d0739947f53
Vladzimir Kuznetsov <VladzimirKuznetsov@coherentsolutions.com>
2016-11-10 16:59:26 +0300

    added date to configuration

--- Databases/Master/dbo/Tables/Configuration.sql
+++ Databases/Master/dbo/Tables/Configuration.sql
@@ -2,6 +2,7 @@
     [ID]          INT           IDENTITY (1, 1) NOT NULL,
     [OptionName]  VARCHAR (255) NOT NULL,
     [OptionValue] VARCHAR (255) NOT NULL,
+    [DateCreated] DATE NOT NULL, 
     PRIMARY KEY CLUSTERED ([ID] ASC)
 );
 
